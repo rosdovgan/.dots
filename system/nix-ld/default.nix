6c0a6f533080050c09adedb3cab71a2f68f7f35f
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2024-02-09 23:59:48 +0200

    Add to nix-ld some packages

--- system/nix-ld/default.nix
+++ system/nix-ld/default.nix
@@ -3,6 +3,7 @@
   programs.nix-ld.libraries = with pkgs; [ 
        vulkan-loader libglvnd gmp 
        stdenv.cc.cc zlib pulseaudio 
+       icu openssl
     ] ++ 
     (with xorg; [ 
       libX11 libXi libXrandr libXxf86vm libXcursor libXinerama libXext 
