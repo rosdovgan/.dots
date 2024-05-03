6dcc514b28a2fc6f92fe041255fb147211ff32ef
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2024-05-03 10:51:43 +0300

    Stop managing lazy-lock.json

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,17 +1,21 @@
-{ pkgs, c, ... }: {
+{ pkgs, lib, c, ... }: {
   home.packages = with pkgs; [ 
     (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; }) 
   ];
 
   fonts.fontconfig.enable = true;
 
-  xdg.configFile."nvim".source = /${c}/nvim;
-  programs.neovim = { 
+  xdg.configFile = {
+    "nvim/lua".source = /${c}/nvim/lua;
+    "nvim/init.lua".source = /${c}/nvim/init.lua;
+  };
+
+  programs.neovim = {
     enable = true;
     defaultEditor = true;
-    extraPackages = with pkgs; [ 
+    extraPackages = with pkgs; [
       wget gcc zig unzip
-      fd ripgrep tree-sitter 
+      fd ripgrep tree-sitter
       trash-cli
       lua-language-server
       nil
