0eed910237a268e79d468b4b8ce0db2e776b7683
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2024-05-03 10:34:20 +0300

    Add nvim-nio a nvim-dap dependency to neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -107,8 +107,10 @@ require("lazy").setup({
   },
   { "jay-babu/mason-nvim-dap.nvim",
   },
+  { "nvim-neotest/nvim-nio",
+  },
   { "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
   },
 
   -- snippets
