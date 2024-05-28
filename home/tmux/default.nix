26b3c939260aa30221975f3e825c9f5e04a4e32a
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2024-05-28 12:57:15 +0300

    Change tmux's status bar color

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -17,6 +17,7 @@
       + ''
         set -g pane-border-style 'fg=${colors.pureWhite}'
         set -g pane-active-border-style 'fg=${colors.darkBlue}'
+        set -g status-style 'bg=${colors.darkBlue}'
       '';
     plugins = with pkgs.tmuxPlugins; [resurrect];
     tmuxinator.enable = true;
