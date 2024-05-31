dc62ab0dfb769d790ec3e3c0b8d6888516b04eec
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2024-05-31 09:39:01 +0300

    Move nix related options to dedicated module

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,34 +1,47 @@
-{ pkgs, users, ... }: {
+{
+  pkgs,
+  users,
+  ...
+}: {
   imports = [
-    ./nix-ld ./logind ./zsh
+    ./nix
 
-    ./xorg ./xmonad ./lightdm
+    ./nix-ld
+    ./logind
+    ./zsh
+
+    ./xorg
+    ./xmonad
+    ./lightdm
 
     ./binfmt
 
     ./polkit
 
-    ./sound ./nvidia ./opengl
+    ./sound
+    ./nvidia
+    ./opengl
 
     ./locate
 
     ./time-and-locale
 
     ./virtualisation
-    ];
+  ];
 
   environment.systemPackages = with pkgs; [
     where-is-my-sddm-theme
     neovim
-    wget curl
+    wget
+    curl
     git
-    ];
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 50;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [ "ntfs" ];
-  boot.kernelModules = [ "coretemp" ];
+  boot.supportedFilesystems = ["ntfs"];
+  boot.kernelModules = ["coretemp"];
 
   security.allowSimultaneousMultithreading = false;
   boot.initrd.systemd.dbus.enable = true;
@@ -38,8 +51,8 @@
 
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
-      ];
-    config = { common = { default = [ "gtk" ]; }; };
+    ];
+    config = {common = {default = ["gtk"];};};
   };
 
   services.thermald.enable = true;
@@ -64,7 +77,7 @@
     networkmanager.enable = true;
     stevenblack = {
       enable = true;
-      block = [ "fakenews" "gambling" "porn" ];
+      block = ["fakenews" "gambling" "porn"];
     };
   };
 
@@ -72,41 +85,13 @@
     nix-store-packages = "nix-store --query --requisites /run/current-system";
   };
 
-  nix.settings = {
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "ghc-nix.cachix.org-1:wI8l3tirheIpjRnr2OZh6YXXNdK2fVQeOI4SVz/X8nA="
-      "static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE="
-    ];
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://cache.iog.io"
-      "https://ghc-nix.cachix.org"
-      "https://static-haskell-nix.cachix.org"
-      "https://cuda-maintainers.cachix.org"
-      "https://digitallyinduced.cachix.org"
-    ];
-  };
-
   users.users."${users.main.name}" = {
     isNormalUser = true;
     description = users.main.description;
-    extraGroups = [ "networkmanager" "audio" "video" "wheel" "plocate" ];
+    extraGroups = ["networkmanager" "audio" "video" "wheel" "plocate"];
     shell = pkgs.zsh;
   };
 
-  nix.gc = {
-    automatic = true;
-    dates = "monthly";
-    options = "--delete-older-than 3month";
-  };
-
-  nix.settings.experimental-features = "nix-command flakes";
   nixpkgs.config.allowUnfree = true;
 
   # man configuration.nix or on https://nixos.org/nixos/options.html
