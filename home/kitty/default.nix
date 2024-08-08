ff1036b6d05b0bf845da8f7ba581243c6d4d5e3a
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2024-08-08 16:57:23 +0300

    Changes to kitty's colors

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -1,90 +1,71 @@
-{ c, colors, ... }: {
-  programs.kitty = { 
+{
+  c,
+  colors,
+  ...
+}: {
+  programs.kitty = {
     enable = true;
     shellIntegration.enableZshIntegration = true;
-    extraConfig = builtins.readFile /${c}/kitty/kitty.conf + ''
-      background                ${colors.blackT10}
-      foreground                ${colors.white}
+    extraConfig =
+      builtins.readFile /${c}/kitty/kitty.conf
+      + ''
+        background                ${colors.blackT10}
+        foreground                ${colors.white}
 
-      selection_background      ${colors.blackT11}
-      selection_foreground      ${colors.grayT35}
+        selection_background      ${colors.blackT11}
+        selection_foreground      ${colors.grayT35}
 
-      url_color                 ${colors.neutralBlue}
+        url_color                 ${colors.neutralBlue}
 
-      cursor                    ${colors.grayT35}
-      cursor_text_color         ${colors.black}
+        cursor                    ${colors.grayT35}
+        cursor_text_color         ${colors.black}
 
-      active_border_color       ${colors.blackT11}
-      inactive_border_color     ${colors.blackT17}
-      bell_border_color         ${colors.neutralRed}
+        active_border_color       ${colors.blackT11}
+        inactive_border_color     ${colors.blackT17}
+        bell_border_color         ${colors.neutralRed}
 
-      active_tab_background     ${colors.blackT10}
-      active_tab_foreground     ${colors.whiteS01}
-      inactive_tab_background   ${colors.blackT11}
-      inactive_tab_foreground   ${colors.grayS15}
+        active_tab_background     ${colors.blackT10}
+        active_tab_foreground     ${colors.whiteS01}
+        inactive_tab_background   ${colors.blackT11}
+        inactive_tab_foreground   ${colors.grayS15}
 
-      # Black
-      color0                    ${colors.black}
-      color8                    ${colors.grayS15}
+        # Black
+        color0                    ${colors.black}
+        color8                    ${colors.grayS15}
 
-      # Red
-      color9                    ${colors.neutralRed}
-      color1                    ${colors.lightRed}
+        # Red
+        color1                    ${colors.lightRed}
+        color9                    ${colors.lightRed}
+        # color9                    ${colors.neutralRed}
 
-      # Green
-      color10                   ${colors.neutralGreen}
-      color2                    ${colors.lightGreen}
+        # Green
+        color2                    ${colors.lightGreen}
+        color10                   ${colors.lightGreen}
+        # color10                   ${colors.neutralGreen}
 
-      # Yellow
-      color11                   ${colors.neutralYellow}
-      color3                    ${colors.lightYellow}
+        # Yellow
+        color3                    ${colors.lightYellow}
+        color11                   ${colors.lightYellow}
+        # color11                   ${colors.neutralYellow}
 
-      # Blue
-      color12                   ${colors.neutralBlue}
-      color4                    ${colors.lightBlue}
+        # Blue
+        color4                    ${colors.lightBlue}
+        color12                   ${colors.lightBlue}
+        # color12                   ${colors.neutralBlue}
 
-      # Magenta
-      color13                   ${colors.neutralPurple}
-      color5                    ${colors.lightPurple}
+        # Magenta
+        color5                    ${colors.lightPurple}
+        color13                   ${colors.lightPurple}
+        # color13                   ${colors.neutralPurple}
 
-      # Cyan
-      color14                   ${colors.neutralCyan}
-      color6                    ${colors.lightCyan}
+        # Cyan
+        color6                    ${colors.lightCyan}
+        color14                   ${colors.lightCyan}
+        # color14                   ${colors.neutralCyan}
 
-      # White
-      color7                    ${colors.grayT35}
-      color15                   ${colors.whiteS01}
-    '';
+        # White
+        color7                    ${colors.grayT35}
+        color15                   ${colors.whiteS01}
+      '';
   };
 }
-# # Black
-# color0                    ${colors.black}
-# color8                    ${colors.grayS15}
-#
-# # Red
-# color1                    ${colors.red}
-# color9                    ${colors.lightRed}
-#
-# # Green
-# color2                    ${colors.green}
-# color10                   ${colors.lighterGreen}
-#
-# # Yellow
-# color3                    ${colors.yellow}
-# color11                   ${colors.lightYellow}
-#
-# # Blue
-# color4                    ${colors.cyan}
-# color12                   ${colors.lightGreen}
-#
-# # Magenta
-# color5                    ${colors.yellow}
-# color13                   ${colors.lightYellow}
-#
-# # Cyan
-# color6                    ${colors.blue}
-# color14                   ${colors.lightBlue}
-#
-# # White
-# color7                    ${colors.grayT35}
-# color15                   ${colors.whiteS01}
