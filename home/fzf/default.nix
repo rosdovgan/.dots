63c29026a86db9e0516e82520a7edc660d4565e4
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2025-02-08 20:07:06 +0200

    Adjust fzf

--- home/fzf/default.nix
+++ home/fzf/default.nix
@@ -1,6 +1,27 @@
-{pkgs, ...}: {
-  programs.fzf = {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.fzf = let
+    fd = "${pkgs.fd}/bin/fd";
+    fdh = fd + " --hidden";
+    keyBindings = "down:toggle+down,up:toggle+up,tab:down,shift-tab:up";
+  in {
     enable = true;
-    defaultCommand = "${pkgs.fd}/bin/fd --hidden";
+    defaultCommand = fdh;
+    defaultOptions = ["--bind \"${keyBindings}\"" "--color 16"];
+
+    # Alt + T
+    fileWidgetCommand =
+      fdh
+      + " . ${config.home.homeDirectory}";
+    fileWidgetOptions = [];
+
+    # Alt + C
+    changeDirWidgetCommand =
+      fdh
+      + " --type d";
+    changeDirWidgetOptions = [];
   };
 }
