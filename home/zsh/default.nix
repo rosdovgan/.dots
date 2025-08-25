8e0a0a641c3ef1eba7d8cabd1d17ec7e2cb987d9
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2025-08-25 18:11:03 +0300

    Flake update

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -6,7 +6,7 @@
 }: {
   programs.zsh = {
     enable = true;
-    dotDir = ".config/zsh";
+    dotDir = "${config.xdg.configHome}/zsh";
     enableCompletion = true;
     autosuggestion.enable = true;
     history = {
