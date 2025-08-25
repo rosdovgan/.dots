4879b47a8671287bd166505006abb6aff86b623f
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2025-08-25 15:45:52 +0300

    Add xsession execution in xinitrc to fix startx

--- home/home.nix
+++ home/home.nix
@@ -120,10 +120,17 @@
     "user/scripts".source = /${c}/user/scripts;
   };
 
-  xsession.initExtra = ''
+  xsession.initExtra = let
+    xsr = pkgs.xorg.xsetroot + /bin/xsetroot;
+  in ''
+    ${xsr} -cursor_name left_ptr
     telegram-desktop -startintray &
     # slack -u &
     # webcord -m &
+  '';
+
+  home.file.".xinitrc".text = ''
+    exec ~/.xsession
   '';
 
   home.sessionVariables = {
