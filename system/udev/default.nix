56550c6a5e0aaa6ede9f0e3965335b64aace034b
rosdovgan <136788218+rosdovgan@users.noreply.github.com>
2025-09-02 11:41:00 +0300

    Hopefully successful attempt to fix usb keyboard

--- system/udev/default.nix
+++ system/udev/default.nix
@@ -1,21 +1,14 @@
 {...}: {
   # Prevents immediate waking up by a weird usb keyboard behavior
-  # boot.initrd.services.udev.rules = builtins.concatStringsSep ", " [
-  # services.udev.extraRules = builtins.concatStringsSep ", " [
-  #   ''ACTION=="add"''
-  #   ''SUBSYSTEM=="usb"''
-  #   ''DRIVERS=="usb"''
-  #   ''ATTRS{idProduct}=="05c0"''
-  #   ''ATTRS{idVendor}=="c0f4"''
-  #   ''ATTR{power/wakeup}="disabled"''
-  # ];
+  services.udev.extraRules = builtins.concatStringsSep ", " [
+    ''ACTION=="add"''
+    ''SUBSYSTEM=="usb"''
+    ''DRIVERS=="usb"''
+    ''ATTRS{idVendor}=="c0f4"''
+    ''ATTRS{idProduct}=="05c0"''
+    ''ATTR{power/wakeup}="disabled"''
+  ];
 
-  # boot.initrd.services.udev.rules = ''
-  #   ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idProduct}=="05c0", ATTRS{idVendor}=="c0f4", ATTR{power/wakeup}="disabled"
-  # '';
-
-    # ''ATTR{driver/1-4/power/wakeup}="disabled"''
-    # ''ATTRS{power/wakeup}="disabled"''
   # Swap escape and capslock keys
   services.udev.extraHwdb = ''
     evdev:atkbd:*
